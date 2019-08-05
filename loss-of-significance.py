d19690a5bdc4d336796526b9d10b05057edf7f12
nicolasilvac <53713677+nicolasilvac@users.noreply.github.com>
2019-08-05 03:09:08 -0400

    Update loss-of-significance.py

--- loss-of-significance.py
+++ loss-of-significance.py
@@ -8,12 +8,12 @@ L_32=[h_0] #lista 32 bits
 L_64=[h_0] #lista 64 bits
 
 for i in range(15): 
-  H_anterior32=(L_32[-1]) 
-	Inercia32 = b*H_anterior32^3/12
-	L_32.append(np.float32(Inercia32)) 
-	H_anterior64=(L_64[-1]) 
-	Inercia64 = b*H_anterior64^3/12 
-	L_64.append(np.float64(Inercia64))
+   H_anterior32=(L_32[-1]) 
+   Inercia32 = b*H_anterior32^3/12
+   L_32.append(np.float32(Inercia32)) 
+   H_anterior64=(L_64[-1]) 
+   Inercia64 = b*H_anterior64^3/12 
+   L_64.append(np.float64(Inercia64))
 	
 ERROR = []
 for i in range(15): 
